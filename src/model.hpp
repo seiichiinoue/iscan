0aa35bba2e04705e2f8016ca5cf2ff142b198c9a
seiichiinoue <d35inou108@gmail.com>
2021-11-23 17:15:07 +0000

    added new threshold parameter and changed variable names

--- src/model.hpp
+++ src/model.hpp
@@ -76,6 +76,7 @@ public:
     vector<vector<size_t>> _dataset;
     vector<int> _times;
     unordered_map<size_t, int> _word_frequency;
+    unordered_map<int, int> _snippet_count;
 
     double** _logistic_Phi;
     double*** _logistic_Psi;
@@ -88,7 +89,8 @@ public:
     int _year_interval;
 
     int _burn_in_period;
-    int _ignore_word_count;
+    int _min_word_count;
+    int _min_snippet_count;
     int _min_snippet_length;
     int _kappa_phi_interval;
     int _kappa_phi_start;
@@ -121,7 +123,8 @@ public:
         _year_interval = YEAR_INTERVAL;
 
         _burn_in_period = BURN_IN_PERIOD;
-        _ignore_word_count = IGNORE_WORD_COUNT;
+        _min_word_count = MIN_WORD_COUNT;
+        _min_snippet_count = MIN_SNIPPET_COUNT;
         _min_snippet_length = MIN_SNIPPET_LENGTH;
         _kappa_phi_start = KAPPA_PHI_START;
         _kappa_phi_interval = KAPPA_PHI_INTERVAL;
@@ -178,6 +181,7 @@ public:
             words.erase(words.begin());
             _add_document(words, doc_id);
             _times.push_back(time_id);
+            _snippet_count[time_id] += 1;
         }
     }
     void _add_document(vector<wstring> &words, int doc_id) {
@@ -210,7 +214,7 @@ public:
                     if (_times[n] != t || _scan->_Z[n] != k) continue;
                     for (int i=0; i<_dataset[n].size(); ++i) {
                         size_t word_id = _dataset[n][i];
-                        if (_word_frequency[word_id] < _ignore_word_count) {
+                        if (_word_frequency[word_id] < _min_word_count) {
                             continue;
                         }
                         cnt_t_k[word_id]++;
@@ -302,8 +306,11 @@ public:
     void set_burn_in_period(int burn_in_period) {
         _burn_in_period = burn_in_period;
     }
-    void set_ignore_word_count(int ignore_word_count) {
-        _ignore_word_count = ignore_word_count;
+    void set_min_word_count(int min_word_count) {
+        _min_word_count = min_word_count;
+    }
+    void set_min_snippet_count(int min_snippet_count) {
+        _min_snippet_count = min_snippet_count;
     }
     void set_min_snippet_length(int min_snippet_length) {
         _min_snippet_length = min_snippet_length;
@@ -311,7 +318,7 @@ public:
     int get_sum_word_frequency() {
         int sum = 0;
         for (int v=0; v<_vocab->num_words(); ++v) {
-            if (_word_frequency[v] >= _ignore_word_count) {
+            if (_word_frequency[v] >= _min_word_count) {
                 sum += _word_frequency[v];
             }
         }
@@ -323,7 +330,7 @@ public:
     int get_ignore_word_size() {
         int cnt = 0;
         for (int v=0; v<_word_frequency.size(); ++v) {
-            if (_word_frequency[v] < _ignore_word_count) {
+            if (_word_frequency[v] < _min_word_count) {
                 cnt++;
             }
         }
@@ -345,7 +352,7 @@ public:
             for (int k=0; k<_scan->_n_k; ++k) {
                 for (int i=0; i<_dataset[n].size(); ++i) {
                     size_t word_id = _dataset[n][i];
-                    if (_word_frequency[word_id] < _ignore_word_count) {
+                    if (_word_frequency[word_id] < _min_word_count) {
                         continue;
                     }
                     probs_n[k] += log(logistic_psi_t[k][word_id]);
@@ -450,7 +457,7 @@ public:
                 if (_times[n] != t || _scan->_Z[n] != k) continue;
                 for (int i=0; i<_dataset[n].size(); ++i) {
                     size_t word_id = _dataset[n][i];
-                    if (_word_frequency[word_id] < _ignore_word_count) {
+                    if (_word_frequency[word_id] < _min_word_count) {
                         continue;
                     }
                     cnt_t_k[word_id]++;
@@ -459,13 +466,13 @@ public:
             }
             double denom = 0.0;
             for (int v=0; v<_scan->_vocab_size; ++v) {
-                if (_word_frequency[v] < _ignore_word_count) {
+                if (_word_frequency[v] < _min_word_count) {
                     continue;
                 }
                 denom += exp(psi_t_k[v]);
             }
             for (int v=0; v<_scan->_vocab_size; ++v) {
-                if (_word_frequency[v] < _ignore_word_count) {
+                if (_word_frequency[v] < _min_word_count) {
                     continue;
                 }
                 double constants = denom - exp(psi_t_k[v]);
@@ -489,7 +496,7 @@ public:
             // sanity check
             double sum = 0;
             for (int v=0; v<_scan->_vocab_size; ++v) {
-                if (_word_frequency[v] < _ignore_word_count) continue;
+                if (_word_frequency[v] < _min_word_count) continue;
                 sum += _logistic_Psi[t][k][v];
             }
             assert(abs(1.0 - sum) < 1e-5);
@@ -569,7 +576,7 @@ public:
         double u = 0.0;
         bool init_flag = 1;
         for (int v=0; v<_scan->_vocab_size; ++v) {
-            if (_word_frequency[v] < _ignore_word_count) {
+            if (_word_frequency[v] < _min_word_count) {
                 continue;
             }
             u = logsumexp(u, psi_t_k[v], init_flag);
@@ -600,7 +607,7 @@ public:
                     double log_pw_dk = log(_logistic_Phi[t][k]);
                     for (int i=0; i<_dataset[n].size(); ++i) {
                         size_t word_id = _dataset[n][i];
-                        if (_word_frequency[word_id] < _ignore_word_count) {
+                        if (_word_frequency[word_id] < _min_word_count) {
                             continue;
                         }
                         log_pw_dk += log(_logistic_Psi[t][k][word_id]);
@@ -647,7 +654,9 @@ public:
         oarchive << _end_year;
         oarchive << _year_interval;
         oarchive << _burn_in_period;
-        oarchive << _ignore_word_count;
+        oarchive << _min_word_count;
+        oarchive << _min_snippet_count;
+        oarchive << _min_snippet_length;
         oarchive << _kappa_phi_start;
         oarchive << _kappa_phi_interval;
         oarchive << _current_iter;
@@ -668,7 +677,9 @@ public:
             iarchive >> _end_year;
             iarchive >> _year_interval;
             iarchive >> _burn_in_period;
-            iarchive >> _ignore_word_count;
+            iarchive >> _min_word_count;
+            iarchive >> _min_snippet_count;
+            iarchive >> _min_snippet_length;
             iarchive >> _kappa_phi_start;
             iarchive >> _kappa_phi_interval;
             iarchive >> _current_iter;
--- train.cpp
+++ train.cpp
@@ -9,19 +9,21 @@ DEFINE_double(gamma_b, 3.0, "hyperparameter of gamma prior");
 DEFINE_int32(kappa_phi_start, 100, "start point of kappa sampling");
 DEFINE_int32(kappa_phi_interval, 50, "interval of kappa sampling");
 DEFINE_double(scaling_coeff, 1.0, "concentration parameter of LSBP");
-DEFINE_double(sigma_coeff, 0.1, "random walk width of MH sampling");
+DEFINE_double(sigma_coeff, 0.05, "random walk width of MH sampling");
 DEFINE_int32(start_year, 1700, "start year in the corpus");
 DEFINE_int32(end_year, 2020, "end year in the corpus");
 DEFINE_int32(year_interval, 20, "year interval");
 DEFINE_int32(context_window_width, 10, "context window width");
 DEFINE_int32(num_iteration, 1000, "number of iteration");
 DEFINE_int32(burn_in_period, 500, "burn in period");
-DEFINE_int32(ignore_word_count, 3, "threshold of low-frequency words");
+DEFINE_int32(min_word_count, 10, "threshold of low-frequency words");
+DEFINE_int32(min_snippet_count, 100, "threshold for snippets size in the time point")
 DEFINE_int32(min_snippet_length, 3, "threshold of size of snippet");
 DEFINE_string(data_path, "./data/transport/corpus.txt", "path to dataset for training");
 DEFINE_string(save_path, "./bin/scan.model", "path to model for archive");
 DEFINE_string(load_path, "./bin/scan.model", "path to model for loading");
 DEFINE_bool(from_archive, false, "load archive or not");
+DEFINE_bool(mle_initialize, false, "use mle for parameter initialization or not");
 
 int main(int argc, char *argv[]) {
     google::InitGoogleLogging(*argv);
@@ -42,12 +44,13 @@ int main(int argc, char *argv[]) {
     trainer.set_year_interval(FLAGS_year_interval);
     trainer.set_context_window_width(FLAGS_context_window_width);
     trainer.set_burn_in_period(FLAGS_burn_in_period);
-    trainer.set_ignore_word_count(FLAGS_ignore_word_count);
+    trainer.set_min_word_count(FLAGS_min_word_count);
+    trainer.set_min_snippet_count(FLAGS_min_snippet_count);
     trainer.set_min_snippet_length(FLAGS_min_snippet_length);
     // load dataset
     trainer.load_documents(FLAGS_data_path);
     // prepare model
-    trainer.prepare();
+    trainer.prepare(FLAGS_mle_initialize);
     // load archive if from_archive is true
     if (FLAGS_from_archive) {
         trainer.load(FLAGS_load_path);
@@ -62,7 +65,8 @@ int main(int argc, char *argv[]) {
         << ", context_window_width: " << trainer._scan->_context_window_width
         << ", num_iteration: " << FLAGS_num_iteration
         << ", burn_in_period: " << FLAGS_burn_in_period
-        << ", ignore_word_count: " << FLAGS_ignore_word_count
+        << ", min_word_count: " << FLAGS_min_word_count
+        << ", min_snippet_count: " << FLAGS_min_snippet_count
         << ", min_snippet_length: " << FLAGS_min_snippet_length << "}" << endl;
     cout << "num of docs: " << trainer._scan->_num_docs << endl;
     cout << "sum of word freq: " << trainer.get_sum_word_frequency() << endl;
