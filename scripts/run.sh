8b8ea53f4b367fa4cfa797cc09133d205085b0c5
seiichiinoue <d35inou108@gmail.com>
2022-04-26 13:54:12 +0000

    change vocab truncation method

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,8 +5,7 @@ KAPPA_PSI=100.0
 KAPPA_PHI_START=100
 KAPPA_PHI_INTERVAL=50
 MIN_SNIPPET_LENGTH=1
-TOP_N_WORD=3000
-# TOP_N_WORD=500
+MIN_WORD_COUNT=5
 DAY=$(date "+%m%d")
 
 # for japanese corpus settings (year)
@@ -32,7 +31,7 @@ LOG_PATH=./results/log/${TARGET_WORD}_${DAY}
        -kappa_phi_start=$KAPPA_PHI_START \
        -kappa_phi_interval=$KAPPA_PHI_INTERVAL \
        -min_snippet_length=$MIN_SNIPPET_LENGTH \
-       -top_n_word=$TOP_N_WORD \
+       -min_word_count=$MIN_WORD_COUNT \
        -start_year=$START_YEAR \
        -end_year=$END_YEAR \
        -year_interval=$YEAR_INTERVAL \
