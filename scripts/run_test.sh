f5688542219e3ec39f46a0bf47950622290569ca
seiichi.inoue <d35inou108@gmail.com>
2022-07-09 22:26:27 +0000

    changed params

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,43 +1,36 @@
 # parameters
 # sampler
 NUM_TIMES=10
-NUM_SENSES=5
-CONTEXT_WINDOW_SIZE=10
-VOCAB_SIZE_PER_SENSE=1000
-RATIO_COMMON_VOCAB=0.0
-NUM_SAMPLE_PER_TIME=500
+NUM_SENSES=$1
+VOCAB_SIZE=10000
+NUM_SAMPLE=10000
 SHIFT_TYPE="random"
 WORD_PRIOR_TYPE="zipf"
 
-# sb-scan
+# iscan
 NUM_ITERATION=1000
-SCALING_COEFF=1.0
 KAPPA_PSI=100.0
-MIN_WORD_COUNT=10
+TOP_N_WORD=$2
 
 DAY=$(date "+%m%d")
-INPUT_DATA=./data/pseudo/pseudo_${WORD_PRIOR_TYPE}_sense${NUM_SENSES}_vocab${VOCAB_SIZE_PER_SENSE}_common${RATIO_COMMON_VOCAB}_window${CONTEXT_WINDOW_SIZE}_sample${NUM_SAMPLE_PER_TIME}.txt
-SUFFIX=pseudo_${WORD_PRIOR_TYPE}_sense${NUM_SENSES}_sample${NUM_SAMPLE_PER_TIME}_vocab${VOCAB_SIZE_PER_SENSE}_kappa_psi${KAPPA_PSI}_min_word${MIN_WORD_COUNT}_${DAY}
+INPUT_DATA=./data/pseudo_v2/pseudo_${WORD_PRIOR_TYPE}_sense${NUM_SENSES}_vocab${VOCAB_SIZE}_sample${NUM_SAMPLE}.txt
+SUFFIX=pseudo_${WORD_PRIOR_TYPE}_sense${NUM_SENSES}_sample${NUM_SAMPLE}_vocab${VOCAB_SIZE}_kappa_psi${KAPPA_PSI}_min_word${MIN_WORD_COUNT}_${DAY}
 BINARY_PATH=./results/bin/${SUFFIX}.model
 LOG_PATH=./results/log/${SUFFIX}
 
 # generate pseudo data
 python3 tools/create_pseudo_data.py --num-times $NUM_TIMES \
                                     --num-senses $NUM_SENSES \
-                                    --context-window-size $CONTEXT_WINDOW_SIZE \
-                                    --vocab-size-per-sense $VOCAB_SIZE_PER_SENSE \
-                                    --ratio-common-vocab $RATIO_COMMON_VOCAB \
-                                    --num-sample $NUM_SAMPLE_PER_TIME \
+                                    --vocab-size $VOCAB_SIZE \
+                                    --num-sample $NUM_SAMPLE \
                                     --shift-type $SHIFT_TYPE \
                                     --word-prior-type $WORD_PRIOR_TYPE \
                                     --output-path $INPUT_DATA
 # execute test
 ./scan -data_path=$INPUT_DATA \
        -save_path=$BINARY_PATH \
-       -context_window_width=$CONTEXT_WINDOW_SIZE \
-       -scaling_coeff=$SCALING_COEFF \
        -kappa_psi=$KAPPA_PSI \
-       -min_word_count=$MIN_WORD_COUNT \
+       -top_n_word=$TOP_N_WORD \
        -start_year=0 \
        -end_year=$NUM_TIMES \
        -year_interval=1 \
